2a5363d37e65f360768d92277fd0289133533945
Croix Kimmel <croixkimmel@gmail.com>
2022-02-16 12:41:29 -0700

    Modify test for authentication

--- spec/features/user/new_spec.rb
+++ spec/features/user/new_spec.rb
@@ -2,12 +2,24 @@ require 'rails_helper'
 
 RSpec.describe 'User new page' do
 
-  it 'has a form for new users' do
+  # it 'has a form for new users' do
+  #   visit register_path
+  #   fill_in("Name", with: "Name_1")
+  #   fill_in("Email", with: "Name_1@email.com")
+  #   click_button "Register"
+  #   expected = User.last
+  #   expect(current_path).to eq("/users/#{expected.id}")
+  # end
+
+  it 'has a form for new users - authentication' do
     visit register_path
-    fill_in("Name", with: "Name_1")
-    fill_in("Email", with: "Name_1@email.com")
+    fill_in("username", with: "username_1")
+    fill_in("password", with: "password_1")
     click_button "Register"
     expected = User.last
     expect(current_path).to eq("/users/#{expected.id}")
   end
+
+
+
 end
